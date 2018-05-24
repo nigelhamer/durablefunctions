198fc671f9b8e3d3ff28c122e5c93650d508fc8a
Nigel Hamer <nigel.hamer@capgemini.com>
2018-05-24 15:17:26 +0100

    Initial Commit

---
+++ CompanyFunctionApp/Activities/SayHello.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+using Microsoft.Azure.WebJobs;
+
+namespace CompanyFunctionApp.Activities
+{
+    public static class SayHello
+    {
+        [FunctionName("E1_SayHello")]
+        public static string Run([ActivityTrigger] string name)
+        {
+            return $"Hello {name}!";
+        }
+    }
+}
---
+++ CompanyFunctionApp/Orchestrations/HelloSequence.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace CompanyFunctionApp.Orchestration
+{
+    public static class HelloSequence
+    {
+        [FunctionName("E1_HelloSequence")]
+        public static async Task<List<string>> Run(
+            [OrchestrationTrigger] DurableOrchestrationContextBase context)
+        {
+            var outputs = new List<string>();
+
+            outputs.Add(await context.CallActivityAsync<string>("E1_SayHello", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("E1_SayHello", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("E1_SayHello", "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+    }
+}
